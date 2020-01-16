a1f3cfea0b202e1c1b6b892ba8c2b4da28e631f3
Jochen Vothknecht <jochen3120@gmail.com>
2020-01-16 14:45:10 +0100

    documentation

--- src/main/java/org/dice_research/opal/licenses/LicenseAttribute.java
+++ src/main/java/org/dice_research/opal/licenses/LicenseAttribute.java
@@ -7,6 +7,12 @@ package org.dice_research.opal.licenses;
  */
 public interface LicenseAttribute {
 
+	/**
+	 * Used in searching for compatible licenses.
+	 * 
+	 * If License A has Permission P, than License B is compatible to A if it either permits P or not.
+	 * If A doesn't permit P, than B can't permit P either.
+	 */
 	public enum Permission implements LicenseAttribute {
 		REPRODUCTION {
 			public String toString() {
@@ -27,6 +33,12 @@ public interface LicenseAttribute {
 		PATENTGRANT
 	}
 	
+	/**
+	 * Used in searching for compatible licenses.
+	 * 
+	 * If License A has Requirement R, than License B is compatible to A if it requires R too.
+	 * If A doesn't require R, than B may or may not require R.
+	 */
 	public enum Requirement implements LicenseAttribute {
 		NOTICE {
 			public String toString() {
@@ -56,6 +68,12 @@ public interface LicenseAttribute {
 		STATECHANGES
 	}
 	
+	/**
+	 * Used in searching for compatible licenses.
+	 * 
+	 * If License A has Prohibition P, than License B is compatible to A if it prohibits P too.
+	 * If A doesn't prohibit P, than B may or may not prohibit P.
+	 */
 	public enum Prohibition implements LicenseAttribute {
 		COMMERCIAL {
 			public String toString() {
