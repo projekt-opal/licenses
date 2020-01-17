a28d5784224bcd62a673b97a3ef009a11c3622e5
Jochen Vothknecht <jochen3120@gmail.com>
2020-01-17 13:40:27 +0100

    more examples

--- src/test/java/org/dice_research/opal/licenses/example/Example.java
+++ src/test/java/org/dice_research/opal/licenses/example/Example.java
@@ -1,8 +1,11 @@
 package org.dice_research.opal.licenses.example;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
+import org.dice_research.opal.licenses.LicenseAttribute;
 import org.dice_research.opal.licenses.LicenseCombinator;
 import org.dice_research.opal.licenses.LicenseCombinatorInterface;
 import org.dice_research.opal.licenses.exceptions.UnknownLicenseException;
@@ -10,10 +13,9 @@ import org.dice_research.opal.licenses.exceptions.UnknownLicenseException;
 
 public class Example {
 	
-	public void combineLicenses() throws UnknownLicenseException {
-		// Create LicenseCombinator
 		LicenseCombinatorInterface lc = new LicenseCombinator();
 		
+	public void combineLicenses() throws UnknownLicenseException {
 		// Put some license-URIs in a collection
 		Collection<String> licenses = new LinkedList<>();
 		licenses.add("https://creativecommons.org/publicdomain/zero/1.0/legalcode");
@@ -21,5 +23,21 @@ public class Example {
 		
 		// Yields a list containing only "https://www.govdata.de/dl-de/by-2-0"
 		lc.getLicenseSuggestions(licenses);
+	}
+	
+	/**
+	 * Search for licenses with specific attributes
+	 */
+	public void licenseFromAttributes() {
+		
+		// create attribute map
+		Map<LicenseAttribute, Boolean> attributes = new HashMap<>();
+		
+		// add some attributes
+		attributes.put(LicenseAttribute.Permission.DERIVATIVE, true);
+		attributes.put(LicenseAttribute.Permission.SUBLICENSING, true);
+		
+		// Yields 12 different licenses which all allow derivative works and sublicensing
+		lc.getLicenseFromAttributes(attributes);
 	}
 }
